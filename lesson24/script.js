7490e035265b6e1ac353651aa46aeec9fca83e64
CPU <cpusyntax@gmail.com>
2019-03-23 11:18:32 -0400

    add batch

--- lesson24/script.js
+++ lesson24/script.js
@@ -1,10 +1,11 @@
-document.querySelector("button").addEventListener("click", function(){
-  document.body.style.background = randColor();
+document.querySelector("button").addEventListener("click", function () {
+    document.body.style.background = randColor();
 })
-
-function randColor(){
-  return '#' + (function co(lor){   
-      return (lor +=     [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])  && (lor.length == 6) ?  lor : co(lor); })('');
+console.log("Yo BB");
+function randColor() {
+    return '#' + (function co(lor) {
+        return (lor += [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random() * 16)]) && (lor.length == 6) ? lor : co(lor);
+    })('');
 }
 // document.write("<html><body><p>Hello World</p></body></html>")
 // document.open("text/html", "replace");
@@ -14,3 +15,4 @@ document.write("<h1>Hello Everyone</h1>");
 document.write(2 + 2);
 // document.write(<p>hello</p>);
 // document.close();
+// if var a !=== var b
